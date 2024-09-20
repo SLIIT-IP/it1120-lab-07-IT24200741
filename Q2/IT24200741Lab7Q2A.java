8a2e502505bf4c48eed8f74f2512f261458a1767
IT24200741 <it24200741@my.sliit.lk>
2024-09-20 14:10:32 +0530

    Add Lab7Q2A

---
+++ Q2/IT24200741Lab7Q2A.java
@@ -0,0 +1,14 @@
+public class IT24200741Lab7Q2A{
+	public static void main(String[] args){
+		int length = 5;
+		int width = 4;
+		
+		for(int i = 0; i < width; i++){
+		    for(int j = 0; j < length; j++){
+    			System.out.print("$ ");
+		    }
+		    
+		    System.out.println();
+		}
+	}
+}
